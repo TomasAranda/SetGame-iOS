da6cad09509f722a122efdd17089c09b6dfc60a6
Tomás Aranda <tomas.aranda.95@gmail.com>
2021-12-01 12:06:52 -0300

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Roberto Aranda on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
