6fa79e676f11846ccaf2251deb5cc4016aa74845
Tomás Aranda <tomas.aranda.95@gmail.com>
2021-12-06 20:47:53 -0300

    Add deck generation logic

--- Set Game/Model/SetGame.swift
+++ Set Game/Model/SetGame.swift
@@ -14,6 +14,10 @@ struct SetGame {
         get { cards.filter { $0.isSelected } }
     }
     
+    init (cards: Array<Card>) {
+        self.cards = SetGame.generateCardDeck()
+    }
+    
     // select card
     mutating func choose(_ card: Card) {
         if let choosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
@@ -21,29 +25,29 @@ struct SetGame {
         }
     }
         
+    static func isSet(cards: Array<Card>) -> Bool {
+        guard cards.count == 3 else {
+            return false
+        }
+        
+        let sumOfRawValues = cards.reduce(into: [0, 0, 0, 0], { sum, card in
+            let rawValues = card.attributes.toArrayOfRawValues()
+            for (index, value) in rawValues.enumerated() {
+                sum[index] += value
+            }
+        })
+        
+        return sumOfRawValues.allSatisfy { value in
+            value % 3 == 0
+        }
+    }
+    
     struct Card: Identifiable {
-        var id: Int
+        let id: Int
         let attributes: CardAttributes
         
         var isSelected = false
         var isMatched = false
-        
-        static func match(cards: Array<Card>) -> Bool {
-            guard cards.count == 3 else {
-                return false
-            }
-            
-            let sumOfRawValues = cards.reduce(into: [0, 0, 0, 0], { sum, card in
-                let rawValues = card.attributes.toArrayOfRawValues()
-                for (index, value) in rawValues.enumerated() {
-                    sum[index] += value
-                }
-            })
-            
-            return sumOfRawValues.allSatisfy { value in
-                value % 3 == 0
-            }
-        }
     }
 
     struct CardAttributes {
@@ -51,15 +55,32 @@ struct SetGame {
         let shape: CardAttributeVariant
         let color: CardAttributeVariant
         let shading: CardAttributeVariant
+    }
+}
+
+extension SetGame {
+    private static func generateCardDeck() -> Array<Card> {
+        var deck = Array<Card>()
         
-        func toArrayOfRawValues() -> [Int] {
-            return [
-                self.number.rawValue,
-                self.shape.rawValue,
-                self.color.rawValue,
-                self.shading.rawValue
-            ]
+        for number in CardAttributeVariant.allCases {
+            for shape in CardAttributeVariant.allCases {
+                for color in CardAttributeVariant.allCases {
+                    for shading in CardAttributeVariant.allCases {
+                        deck.append(Card(
+                            id: 1,
+                            attributes: CardAttributes(
+                                number: number,
+                                shape: shape,
+                                color: color,
+                                shading: shading
+                            )
+                        ))
+                    }
+                }
+            }
         }
+        
+        return deck
     }
 }
 
@@ -67,3 +88,13 @@ enum CardAttributeVariant: Int, CaseIterable {
     case one = 1, two, three
 }
 
+extension SetGame.CardAttributes {
+    func toArrayOfRawValues() -> [Int] {
+        return [
+            self.number.rawValue,
+            self.shape.rawValue,
+            self.color.rawValue,
+            self.shading.rawValue
+        ]
+    }
+}
